ac263659cd6ff63f68ab070ca98ef9469da92de1
Rita Linets <rita.linets@gmail.com>
2022-11-17 22:10:54 -0500

    add avatar

--- src/components/auth/AuthenticationButton.tsx
+++ src/components/auth/AuthenticationButton.tsx
@@ -1,10 +1,10 @@
 import LoginButton from './LoginButton';
-import LogoutButton from './LogoutButton';
 import { useAuth0 } from '@auth0/auth0-react';
+import UserAvatar from '../profile/UserAvatar';
 
 const AuthenticationButton = () => {
     const { isAuthenticated } = useAuth0();
-    return isAuthenticated ? <LogoutButton /> : <LoginButton />;
+    return isAuthenticated ? <UserAvatar /> : <LoginButton />;
 };
 
 export default AuthenticationButton;
